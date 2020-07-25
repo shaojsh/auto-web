19dbcf81e3b3562f358eec769f05e83ed2ddf384
junshuai shao <shaojunshuai2019@sina.com>
2020-07-25 18:49:30 +0800

    二级菜单做成

--- AutoWeb/settings.py
+++ AutoWeb/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'AutoWeb-PA.urls'
+ROOT_URLCONF = 'AutoWeb.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'AutoWeb-PA.wsgi.application'
+WSGI_APPLICATION = 'AutoWeb.wsgi.application'
 
 
 # Database
--- AutoWeb/wsgi.py
+++ AutoWeb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AutoWeb-PA.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AutoWeb.settings")
 
 application = get_wsgi_application()
